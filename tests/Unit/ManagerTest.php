9ae47688cb6b8e2439ee39529c6637b0d6b1d8c2
Oliver Hader <oliver@typo3.org>
2018-08-26 22:50:21 +0200

    [TASK] Add copyright headers to PHPdoc

--- tests/Unit/ManagerTest.php
+++ tests/Unit/ManagerTest.php
@@ -2,6 +2,16 @@
 declare(strict_types = 1);
 namespace TYPO3\PharStreamWrapper\Tests\Unit;
 
+/*
+ * This file is part of the TYPO3 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under the terms
+ * of the MIT License (MIT). For the full copyright and license information,
+ * please read the LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ObjectProphecy;
 use TYPO3\PharStreamWrapper\Behavior;

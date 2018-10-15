7411bafa4194f929bd4cdac4e4dc17f70f8e8c76
Oliver Hader <oliver@typo3.org>
2018-10-15 21:59:17 +0200

    [FEATURE] Introduce conjunction interceptor

---
+++ tests/Functional/Interceptor/ConjunctionInterceptorTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types = 1);
+namespace TYPO3\PharStreamWrapper\Tests\Functional\Interceptor;
+
+/*
+ * This file is part of the TYPO3 project.
+ *
+ * It is free software; you can redistribute it and/or modify it under the terms
+ * of the MIT License (MIT). For the full copyright and license information,
+ * please read the LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\PharStreamWrapper\Interceptor\ConjunctionInterceptor;
+use TYPO3\PharStreamWrapper\Interceptor\PharExtensionInterceptor;
+use TYPO3\PharStreamWrapper\Interceptor\PharMetaDataInterceptor;
+use TYPO3\PharStreamWrapper\Manager;
+use TYPO3\PharStreamWrapper\PharStreamWrapper;
+
+class ConjunctionInterceptorTest extends AbstractTestCase
+{
+    /**
+     * @var string[]
+     */
+    protected const ALLOWED_PATHS = [
+        __DIR__ . '/../Fixtures/bundle.phar',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected const DENIED_PATHS = [
+        __DIR__ . '/../Fixtures/bundle.phar.png',
+        __DIR__ . '/../Fixtures/serialized.phar',
+        __DIR__ . '/../Fixtures/serialized.phar.gz',
+        __DIR__ . '/../Fixtures/serialized.phar.bz2',
+    ];
+
+    /**
+     * @var int
+     */
+    protected const EXPECTED_EXCEPTION_CODE = 1539625084;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (!in_array('phar', stream_get_wrappers())) {
+            $this->markTestSkipped('Phar stream wrapper is not registered');
+        }
+
+        stream_wrapper_unregister('phar');
+        stream_wrapper_register('phar', PharStreamWrapper::class);
+
+        Manager::initialize(
+            (new \TYPO3\PharStreamWrapper\Behavior())
+                ->withAssertion(new ConjunctionInterceptor([
+                    new PharExtensionInterceptor(),
+                    new PharMetaDataInterceptor(),
+                ]))
+        );
+    }
+
+    protected function tearDown()
+    {
+        stream_wrapper_restore('phar');
+        Manager::destroy();
+        parent::tearDown();
+    }
+}
